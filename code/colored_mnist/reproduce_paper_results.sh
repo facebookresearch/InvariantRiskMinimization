6aad47e689913b9bdad05880833530a5edac389e
David Lopez-Paz <dlp@fb.com>
2019-12-18 12:32:38 +0100

    Headers.

--- code/colored_mnist/reproduce_paper_results.sh
+++ code/colored_mnist/reproduce_paper_results.sh
@@ -1,3 +1,10 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+
 #!/bin/bash
 
 # IRM (the main result). These hyperparameters were chosen by random search
